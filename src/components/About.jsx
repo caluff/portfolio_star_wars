6118cc66d0d17617b6e855f987dcac54daa3b38b
Caluff <dcaluff@gmail.com>
2023-07-22 15:52:11 -0400

    update version 1.0.0 lite. Remove model 3D r2-d2 and star

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,29 +7,28 @@ import {fadeIn, textVariant} from "../utils/motion";
 import {SectionWrapper} from '../hoc';
 import {services} from "../constants";
 
-
 const ServiceCard = ({index, title, icon}) => (
-    <Tilt className="xs:w-[250px] w-full">
-      <motion.div
-        variant={fadeIn("right", "spring", index * 0.5, 0.75)}
-        className="w-full rounded-[20px] shadow-card green-pink-gradient p-[1px]"
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variant={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full rounded-[20px] shadow-card green-pink-gradient p-[1px]"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-black rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
       >
-        <div
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450,
-          }}
-          className="bg-black rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-        >
-          <img className="w-12 h-12 object-contain"
-               src={icon}
-               alt={title}/>
-          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
-        </div>
-      </motion.div>
-    </Tilt>
-  )
+        <img className="w-12 h-12 object-contain"
+             src={icon}
+             alt={title}/>
+        <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+      </div>
+    </motion.div>
+  </Tilt>
+)
 
 const About = () => {
   return (
