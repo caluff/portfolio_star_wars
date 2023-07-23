ab0d7102bb77b1cbd4afebf463dd9ee6ca1070a3
Caluff <dcaluff@gmail.com>
2023-07-23 18:43:04 -0400

    add preload font and upgrade about optimization

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,9 @@
 import logo from "./logo.png";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
+
+import creator from "./creator.jpg";
+import mobile from "./mobile.jpg";
+import web from "./web.jpg";
+
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
@@ -34,7 +35,6 @@ export {
   linkedin,
   telegram,
   logo,
-  backend,
   creator,
   mobile,
   web,
