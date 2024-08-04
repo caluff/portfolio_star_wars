1fe9c017947814db2adfdb2044e504d066bfd43d
Daniel Caluff <dcaluff@gmail.com>
2024-08-04 15:54:08 -0400

    add more analytic

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { inject } from '@vercel/analytics';
 import './index.css'
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
+injectSpeedInsights();
 inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
